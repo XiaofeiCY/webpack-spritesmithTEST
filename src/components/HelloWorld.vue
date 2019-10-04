3c19f1e2e4ee57d423c3fb530860dc3b11861fca
chunyang <1141079850@qq.com>
2019-10-04 17:22:36 +0800

    完成测试

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="demo">
+    <p>第一步：运行npm install --save-dev webpack-spritesmith</p>
+    <p>第二步：先去看build/webpack.base.conf.js文件下关于SpritesmithPlugin配置</p>
+    <p>第三步：index.js文件引入</p>
+    <p>第四步：组件引入，如下：</p>
+    <i class="icon icon-youtube"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.icon {
+  display: inline-block;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
